c550bad145a1319f863c6bfdb11deaba0437e8a8
Jerboa-app <dev@jerboa.app>
2024-01-29 08:31:42 +0000

    path in macos
    Former-commit-id: 08222f4dc017a39ec94014e935b02ae7fa15e41e

--- build.sh
+++ build.sh
@@ -45,8 +45,8 @@ while [[ $# -gt 0 ]]; do
       exit 1
       ;;
     *)
-      POSITIONAL_ARGS+=("$1") # save positional arg
-      shift # past argument
+      POSITIONAL_ARGS+=("$1") 
+      shift 
       ;;
   esac
 done
