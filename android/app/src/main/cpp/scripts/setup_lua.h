df10642eca973a1b3a95e307c8f3cd9e5adb5111
Jerboa-app <dev@jerboa.app>
2024-03-11 10:50:02 +0000

    jiggleometer working in android
    fixes bug in delete (squared length scale)

--- android/app/src/main/cpp/scripts/setup_lua.h
+++ android/app/src/main/cpp/scripts/setup_lua.h
@@ -1,4 +1,39 @@
-static const char * meshes_lua = R"(
+static const char * setup_lua = R"(
+
+config =
+{
+    ["timeStep"]        = 1.0/(4*900.0),   -- physics time step
+    ["subSample"]       = 5,               -- how many physics steps (collisions, movement, etc) per frame
+    ["cofr"]            = 0.25,            -- coefficient of restitution,
+    ["surfaceFriction"] = 0.5              -- friction at surfaces
+}
+
+-- can always call again to reconfigure, the magic of embedded Lua
+hop.configure(config);
+
+jiggleometerScale = 0.015*xmax
+for i = 1,10 do
+
+    r = math.random(255)
+    g = math.random(255)
+    b = math.random(255)
+
+    o = {
+
+        ["transform"] = {0.025+2.0*i*jiggleometerScale,0.975,0.0,1.0},
+        ["colour"] = {r/255,g/255,b/255,1.0},
+        ["moveable"] = false,
+        ["ghost"] = true,
+        ["collisionMesh"] = {{0.0,0.0,jiggleometerScale}},
+        ["name"] = "jiggleometer"..i,
+        ["renderPriority"] = 100000
+
+    }
+    hop.loadObject(o)
+end
+
+math.randomseed(os.time())
+
 meshes = {
     -- L
     --  2
@@ -188,4 +223,7 @@ meshes = {
         {2, 1.5, 0.5, 3}
     }
 }
+
+previewIndex = math.random(#meshes)
+
 )";
